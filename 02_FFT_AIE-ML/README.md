9ecf4ab63e31e63ce524531e2c448c64739fdb4c
xflorentw <florentwerbrouck@hotmail.fr>
2025-11-27 14:26:27 +0000

    Update for tutorial 16

--- 02_FFT_AIE-ML/README.md
+++ 02_FFT_AIE-ML/README.md
@@ -1,15 +1,33 @@
 # DSP Lib 1024-point FFT on AIE-ML
 
+This project is a simple AIE-ML graph implementing a 1024-point FFT using the AMD DSP Library
+
 You can use this project to replicate the steps from the following articles:<br />
 <a href="https://www.hackster.io/florent-werbrouck/14-building-an-fft-on-amd-aie-ml-using-the-dsp-library-a14307">14 Building an FFT on AMD AIE-ML using the DSP Library</a><br />
 <a href="https://www.hackster.io/florent-werbrouck/15-generating-input-txt-files-stimuli-for-aie-ml-from-python-358b9a">15 Generating input txt files stimuli for AIE-ML from Python</a><br />
+<a href="https://www.hackster.io/florent-werbrouck/16-simulate-ai-engine-graphs-from-python-fb171f">16 Simulate AI Engine graphs from Python</a><br />
+
 
 Before running the command to rebuild the project you will need to clone the <a href="https://github.com/Xilinx/Vitis_Libraries">Vitis_Libraries repository</a> and set an environment variable DSPLIB_ROOT pointing to Vitis_Libraries/dsp
 
+## AMD Vitis Workspace
 
 To rebuild the final Vitis Workspace after tutorial 14 run the following command:
 ```
 make all
 ```
+
+To rebuild the worspace with the input data file generated from the Python script as per tutorial 15, run the following command:
+```
+make all VERSION=2
+```
+
+## Vitis Functional Simulation (VFS)
+The AIE-ML graph can be simulated directly from a Python test bench using the AMD Vitis Functional Simulation (VFS) feature as describe in the tutorial 16. To run the VFS simulation using Python3:
+```
+cd vfs/python
+python3 fft1024_dsplib_vfs.py
+```
+Note that you will need to have the Vitis environment set to run the VFS simulation.
 
 <p class="sphinxhide" align="center"><sub>Copyright © 2025 Florent Werbrouck</sub></p>
---
+++ 02_FFT_AIE-ML/vfs/python/fft1024_dsplib_vfs.py
@@ -0,0 +1,131 @@
+#
+# Copyright (C) 2025, Florent Werbrouck. All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+# ------------------------------------------------------------
+# Import Modules
+# ------------------------------------------------------------
+import os
+import vfs
+import numpy as np
+from fxpmath import Fxp
+import varray
+from scipy.fft import fft, fftfreq, fftshift
+import matplotlib.pyplot as plt
+
+#Get DSP Library path
+DSPLIB = os.environ.get("DSPLIB_ROOT")
+if (DSPLIB == None):
+    print("DSPLIB_ROOT environment not set. Please download the Vitis_Libraries repository and point to the dsp folder \n")
+    print("https://github.com/Xilinx/Vitis_Libraries\n")
+    sys.exit()
+else:
+    print("Using DSPLIB from DSPLIB_ROOT environment variable\n")
+
+# Initialize the AIE graph
+myaiefft = vfs.aieGraph(
+    input_file="../../aie/src/14/graph_FFT_1024.cpp",
+    part = "xcve2302-sfva784-1LP-e-S",
+    include_paths=[
+        "../../aie/src/14/",
+        DSPLIB + "/L2/include/aie/",
+        DSPLIB + "/L1/include/aie/",
+        DSPLIB + "/L1/src/aie/"
+    ]
+)
+
+# ------------------------------------------------------------
+# Generate Simulus I/O
+# ------------------------------------------------------------
+#Parameters
+Iterations = 1
+Input_shift = 15;
+N_Taps = 1024
+F1_MHz = 50
+F2_MHz = 150
+Fs_MHz = 400
+
+# Number of sample points
+N_Samp = N_Taps * Iterations
+# sample spacing
+T = 1.0 / Fs_MHz
+A1 = 0.2
+A2 = 0.4;
+
+# Generate Input Signal
+x = np.linspace(0.0, N_Samp*T, N_Samp, endpoint=False)
+tone1 = A1 * np.exp( 1.j * 2.0*np.pi*F1_MHz*x)
+tone2 = A2 * np.exp( 1.j * 2.0*np.pi*F2_MHz*x)
+sig_i = tone1 + tone2;
+
+sig_i_cplx = Fxp(sig_i, dtype='S1.15')
+sig_i_cplx = sig_i_cplx.astype(complex)
+
+# ------------------------------------------------------------
+# FFT
+# ------------------------------------------------------------
+# Set outputs
+python_data = np.zeros(N_Taps * Iterations, dtype=complex)
+aie_data = np.zeros(N_Taps * Iterations, dtype=complex)
+
+# Process each frame with AI Engine X86 Simulation and Python
+for i in range(0, Iterations):
+    input_data = sig_i_cplx[i*1024:(i+1)*1024]
+
+    # Run AIE graph processing
+    y_aie = myaiefft.run(varray.array(input_data*2**Input_shift, varray.cint16))
+    aie_data[i*1024:(i+1)*1024] = np.array(y_aie)
+    
+    # Run Pyton FFT
+    python_data[i*1024:(i+1)*1024] = fft(input_data)
+
+# ------------------------------------------------------------
+# Error Check AIE vs Golden
+# ------------------------------------------------------------
+error_threshold = 1/2**4
+error = 0
+
+python_data = Fxp(python_data, dtype='S10.6')
+aie_data = Fxp(aie_data/2**5, dtype='S10.6')
+
+for i in range(0, aie_data.size):
+    if aie_data[i] != python_data[i]:
+        if(abs(aie_data[i] - python_data[i])> error_threshold):
+            print('Error in sample %d', (i+1))
+            print('Golden:') 
+            print(python_data[i])
+            print('AIE ouput = ')
+            print(aie_data[i])
+            error += 1
+
+if (error<1):
+    print('AI Engine FFT matches the Python FFT within 2 LSB')
+else:
+    print('Test Failed')
+    print('Nb errors : %d' % (error))
+
+# ------------------------------------------------------------
+# Plot signals
+# ------------------------------------------------------------
+xf = fftfreq(N_Taps, T)
+xf = fftshift(xf)   
+yplot = fftshift(python_data[0:N_Taps].astype(complex))
+yplot_aie = fftshift(aie_data[0:N_Taps].astype(complex))
+    
+
+fig, (ax1, ax2,ax3) = plt.subplots(3, 1)
+fig.suptitle('Input / Output signals')
+ax1.plot(x, sig_i.real)
+ax1.set_xlabel('Sample Index')
+ax1.set_ylabel('Real part')
+ax1.set_title('Input signal')
+ax2.plot(xf, np.absolute(yplot))
+ax2.set_xlabel('Frequency (MHz)')
+ax2.set_ylabel('Magnitude')
+ax2.set_title('Output FFT (Python)')
+ax3.plot(xf, np.absolute(yplot_aie))
+ax3.set_xlabel('Frequency (MHz)')
+ax3.set_ylabel('Magnitude')
+ax3.set_title('Output FFT (AIE)')
+fig.tight_layout()
+plt.show()
